b47f4127c9dc6d39b2cebcc13e753a67ac4e40af
ritzz26 <ritagarwalrit@gmail.com>
2023-07-04 17:30:17 +0200

    first commit with working pages

---
+++ public/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDKKsJzJHrjjuP9EfI8Kx8g6MJG_tQt_k8",
+  authDomain: "ritwebsite.firebaseapp.com",
+  projectId: "ritwebsite",
+  storageBucket: "ritwebsite.appspot.com",
+  messagingSenderId: "73456813391",
+  appId: "1:73456813391:web:85945bc3259969b268713a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
